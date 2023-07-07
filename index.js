6b508bfc9958c0ca375934e6f68ad0789de15e47
Abraham Scott <abrahamscott02@gmail.com>
2023-07-07 17:50:31 -0600

    Done

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+divide(2000,100);
+
+const square = parameter => parameter * parameter
+
+square(8,8);
+
+const add = (add1, add2) => add1 + add2
+
+add(1,2);
